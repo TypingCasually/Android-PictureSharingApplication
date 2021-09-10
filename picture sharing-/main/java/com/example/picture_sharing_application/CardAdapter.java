971c31e4febd2b95d473f033fc9a27f1b1c77654
TypingCasually <83198524+TypingCasually@users.noreply.github.com>
2021-09-10 17:27:57 +0800

    整合部分功能（第二次提交）

---
+++ picture sharing-/main/java/com/example/picture_sharing_application/CardAdapter.java
@@ -0,0 +1,174 @@
+package com.example.picture_sharing_application;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import java.util.List;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder>{
+        Bitmap bitmap;
+        private List<Card> mCardList;
+        private Context mContext;
+        private String imgName;
+        //大图会话
+        private Dialog dialog;
+        //显示大图
+        private ImageView mImageView;
+
+        static class ViewHolder extends RecyclerView.ViewHolder{
+            RoundedImageView CardImage;
+            TextView CardContent;
+            RoundedImageView HeadPic;
+            TextView NickName;
+            TextView LikeNumber;
+
+            public ViewHolder(View view){
+                super(view);
+                CardImage = view.findViewById(R.id.iv_image);
+                CardContent = view.findViewById(R.id.tv_content);
+                HeadPic = view.findViewById(R.id.iv_head);
+                NickName = view.findViewById(R.id.tv_name);
+                LikeNumber = view.findViewById(R.id.tv_likeNumber);
+            }
+
+        }
+
+        public CardAdapter(Context context,List<Card> CardList){
+            Log.d("Adapter","适配器创建成功");
+            //super();
+            mContext =context;
+            mCardList = CardList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Log.d("Adapter","适配器视图创建成功");
+            View view = LayoutInflater.from(parent.getContext()).
+                    inflate(R.layout.card_item,parent,false);
+            ViewHolder holder = new ViewHolder(view);
+
+            //图片监听事件
+            holder.CardImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = holder.getAdapterPosition();
+                    Card card = mCardList.get(position);
+                    String imgUrl = card.getPicUrl();
+                    Log.d("Adapter","图片地址为: "+ imgUrl);
+                    //
+                    holder.CardImage.setDrawingCacheEnabled(true);
+                    //之前的已经回收掉了，必须要先创建好
+                    Bitmap img =  Bitmap.createBitmap(holder.CardImage.getDrawingCache());
+                    holder.CardImage.setDrawingCacheEnabled(false);
+                    //获取img名称
+                    imgName = card.getDescription();
+                    //测试
+                    Log.d("Adapter","img的名称为: "+imgName);
+                    Log.d("Adapter","img的图片为: "+img);
+                    mImageView = getImageView(img);
+                    //初始化会话
+                    initDialog();
+                    //显示会话
+                    dialog.show();
+                }
+            });
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Card card = mCardList.get(position);
+            Log.d("Adapter","图片地址为:"+card.getPicUrl());
+            Log.d("Adapter","头像地址为:"+card.getHeadPic());
+            //图片
+            Glide.with(mContext).load(card.getPicUrl())
+                    .into(holder.CardImage);
+            //内容
+            holder.CardContent.setText(card.getDescription());
+            //头像
+            if(card.getHeadPic() != null){
+                Glide.with(mContext).load(card.getHeadPic())
+                        .into(holder.HeadPic);
+            }
+            //昵称
+            holder.NickName.setText(card.getNickName());
+            //收藏数
+            holder.LikeNumber.setText(card.getLikeName());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCardList.size();
+        }
+
+        public void addData(Card card){
+            mCardList.add(card);
+            //notifyDataSetChanged();
+        }
+
+    private void initDialog() {
+//        mImageView = getImageView();
+        //大图所依附的dialog
+        dialog = new Dialog(mContext, R.style.Theme_AppCompat_Light_Dialog);
+        dialog.setContentView(mImageView);
+
+        //大图的点击事件（点击让他消失）
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        //大图的长按监听
+        mImageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                //弹出的“保存图片”的Dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setItems(new String[]{mContext.getResources().getString(R.string.save_picture)}, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //saveCroppedImage(((BitmapDrawable) mImageView.getDrawable()).getBitmap());
+                        Bitmap img = ((BitmapDrawable) mImageView.getDrawable()).getBitmap();
+                        GalleryFileSaver.saveBitmapToGallery(mContext,imgName,img);
+                    }
+                });
+                builder.show();
+                return true;
+            }
+        });
+    }
+
+    //动态的ImageView
+    private ImageView getImageView(Bitmap img){
+        ImageView iv = new ImageView(mContext);
+        //宽高
+        iv.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        //设置Padding
+        iv.setPadding(20,20,20,20);
+        //imageView设置图片
+        iv.setImageBitmap(img);
+        return iv;
+    }
+
+}
---
+++ picture sharing-/main/java/com/example/picture_sharing_application/HomeFragment.java
@@ -0,0 +1,199 @@
+package com.example.picture_sharing_application;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class HomeFragment extends Fragment {
+    private static final String TAG = "HomeFragment";
+    private RecyclerView lvCardList;
+    private List<com.example.picture_sharing_application.Card> CardData;
+    private int page = 1;
+    private int mCurrentColIndex = 0;
+    private com.example.picture_sharing_application.CardAdapter adapter;
+    private Context context = null;
+    //home视图
+    private View rootView;
+    //card视图
+    private View cardView;
+//    //新闻来源
+//    private String source;
+//    //下拉组件
+//    private SwipeRefreshLayout swipe;
+//    //当前新闻页数
+//    private int mPage=1;
+
+//    private int[] mCols = new int[]{Constants.Card_COL5,
+//            Constants.Card_COL7, Constants.Card_COL8,
+//            Constants.Card_COL10, Constants.Card_COL11};
+
+    public HomeFragment(){
+
+    }
+
+    //进行数据请求与解析
+    private okhttp3.Callback callback = new okhttp3.Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+            Log.e(TAG, "Failed to connect server!");
+            e.printStackTrace();
+        }
+
+        @Override
+        public void onResponse(Call call, Response response)
+                throws IOException {
+            if (response.isSuccessful()) {
+                Log.d(TAG,"请求成功!!!");
+                final String body = response.body().string();
+                List<com.example.picture_sharing_application.Card> data;
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Gson gson = new Gson();
+                        Type jsonType =
+                                new TypeToken<com.example.picture_sharing_application.BaseResponse<List<com.example.picture_sharing_application.Card>>>() {}.getType();
+                        com.example.picture_sharing_application.BaseResponse<List<com.example.picture_sharing_application.Card>> CardListResponse =
+                                gson.fromJson(body, jsonType);
+                        for (com.example.picture_sharing_application.Card card:CardListResponse.getData()) {
+                            Log.d(TAG,"数据为:"+card.getDescription());
+                            Log.d(TAG,"头像为:"+card.getHeadPic());
+                            card.updateData();
+                            adapter.addData(card);
+                        }
+
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            } else {
+                Log.d(TAG,"请求失败!!!");
+            }
+        }
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        context = getActivity();
+        rootView = inflater.inflate(R.layout.fragment_home,
+                container, false);
+        cardView = inflater.inflate(R.layout.card_item,
+                container, false);
+        //初始化布局
+        initView();
+        //初始化对象并保存数据
+        initData();
+        return rootView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    //初始化布局
+    private void initView() {
+        lvCardList = rootView.findViewById(R.id.lv_card_list);
+
+//        lvCardList.setOnItemClickListener(
+//                new AdapterView.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> adapterView,
+//                                            View view, int i, long l) {
+//
+//                        Intent intent = new Intent(context,
+//                                DetailActivity.class);
+//
+//                        Card Card = adapter.getItem(i);
+//                        Log.d(TAG, "Card为: " + Card);
+//                        intent.putExtra(Constants.Card_DETAIL_URL_KEY,
+//                                Card.getContentUrl());
+//                        intent.putExtra(Constants.GET_Card_KEY,
+//                                Card);
+//                        startActivity(intent);
+//                    }
+//                });
+
+//        //下拉功能
+//        swipe = rootView.findViewById(R.id.swipe);
+//        swipe.setOnRefreshListener(
+//                new SwipeRefreshLayout.OnRefreshListener() {
+//                    @Override
+//                    public void onRefresh() {
+//                        downRefreshData();
+//                    }
+//                });
+    }
+
+//    //下拉刷新数据
+//    private void downRefreshData() {
+//        mPage++;
+//        Log.d(TAG,"下拉刷新");
+//        adapter.clear();
+//        adapter.notifyDataSetChanged();
+//        refreshData(mPage,source);
+//        swipe.setRefreshing(false);
+//    }
+
+
+    //初始化对象并保存数据
+    private void initData() {
+        CardData = new ArrayList<>();
+//        StaggeredGridLayoutManager layoutManager =
+//                new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        GridLayoutManager layoutManager = new GridLayoutManager(this.context,2);
+        lvCardList.setLayoutManager(layoutManager);
+        adapter = new com.example.picture_sharing_application.CardAdapter(context, CardData);
+        lvCardList.setAdapter(adapter);
+
+        refreshData(1);
+    }
+
+    //实际的API请求(子线程)(异步请求)
+    private void refreshData(final int page) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                com.example.picture_sharing_application.CardRequest requestObj = new com.example.picture_sharing_application.CardRequest();
+
+                requestObj.setNum(com.example.picture_sharing_application.Constants.CARD_NUM);
+                requestObj.setPage(page);
+                String urlParams = requestObj.toString();
+
+                Log.d(TAG,"地址为:"+ com.example.picture_sharing_application.Constants.GENERAL_CARD_URL + urlParams);
+                Request request = new Request.Builder()
+                        .url(com.example.picture_sharing_application.Constants.GENERAL_CARD_URL + urlParams)
+                        .get().build();
+                try {
+                    OkHttpClient client = new OkHttpClient();
+                    client.newCall(request).enqueue(callback);
+                } catch (NetworkOnMainThreadException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+}
